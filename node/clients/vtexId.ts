7adff0370f4db39884bb309184ec53373ac28f20
Elkin Urango <kin3xc@hotmail.com>
2023-12-04 10:01:22 -0500

    feat: add support for headless implementation

--- node/clients/vtexId.ts
+++ node/clients/vtexId.ts
@@ -1,19 +1,25 @@
 import type { InstanceOptions, IOContext } from '@vtex/api'
-import { ExternalClient } from '@vtex/api'
+import { JanusClient } from '@vtex/api'
 
 interface AuthenticatedUser {
   user: string
 }
 
-export default class VtexId extends ExternalClient {
+export default class VtexId extends JanusClient {
   constructor(context: IOContext, options?: InstanceOptions) {
-    super('http://vtexid.vtex.com.br/api/', context, options)
+    super(context, {
+      ...options,
+      headers: {
+        ...options?.headers,
+        'x-vtex-user-agent': context.userAgent,
+      },
+    })
   }
 
   public async getAuthenticatedUser(
     authToken: string
   ): Promise<AuthenticatedUser> {
-    return this.http.get('vtexid/pub/authenticated/user/', {
+    return this.http.get('/api/vtexid/pub/authenticated/user/', {
       metric: 'authenticated-user-get',
       params: { authToken },
     })
