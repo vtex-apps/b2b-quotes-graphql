f79bff1f7294b79b3fca4865d8564a111fb71d36
guidobernal-cubos <guido.bernal@cubos.io>
2025-01-10 10:43:54 -0300

    [Seller Quotes] feat: get seller quotes paginated api (#61)
    * feat: add new markeplace splitting quotes on graphql
    
    * feat: checking new field quotesManagedBy when value is SELLER
    
    * docs: update CHANGELOG
    
    * feat: add configuration for quote creation
    
    * feat: add adjustment for quote seller
    
    * chore: fix prettier errors
    
    * fix: returning ids separated by commas when multiple quotes
    
    * fix: check config and default settings with marketplace option
    
    * feat: new fields for quotes managed by seller
    
    * feat: new fields to save parent quote; change getQuotes query to filter only parent quotes
    
    * feat: create client to notify seller quote
    
    * feat: add changelog
    
    * feat: verify and notify seller quote
    
    * chore: removes console.log from SellerQuotesClient
    
    * docs: update CHANGELOG and prettier fix on markdown
    
    * fix: right splitting of seller quotes
    
    * feat: refactor on creating seller quote map; avoid double check of seller
    
    * refactor: seller quotes client constants
    
    * feat: send creationDate on notify seller quote
    
    * feat: provides a route for seller get a quote by id at marketplace
    
    * docs: update CHANGELOG
    
    * refactor: separating get seller quote into smaller functions
    
    * refactor: function to get org anc cost center names
    
    * refactor: create service class for seller quotes
    
    * fix: removing approvedBySeller field from quote entity
    
    * refactor: renaming seller quotes service to controller
    
    * feat: get seller quotes paginated
    
    * feat: use seller quote controller
    
    * docs: add changelog
    
    * chore: fix misspell
    
    * fix: use array destructuring
    
    * fix: sorting get seller quotes for right pagination
    
    * feat: supporting search and status filters on get seller quotes
    
    * feat: using search filter on quote creator email
    
    * fix: right dependencies
    
    * fix: remove p-limit from root package.json
    
    * feat: suport for custom sort and where on ger seller quotes paginated
    
    * [Seller Quotes] Add new markeplace splitting quotes on graphql (#57)
    
    * feat: add new markeplace splitting quotes on graphql
    
    * docs: update CHANGELOG
    
    * chore: fix prettier errors
    
    * fix: check config and default settings with marketplace option
    
    * docs: update CHANGELOG and prettier fix on markdown
    
    ---------
    
    Co-authored-by: Bruna Santos <brunassdev@gmail.com>
    Co-authored-by: Tiago de Andrade Freire <tiago.freire@cubos.io>
    Co-authored-by: Tiago de Andrade Freire <freire.tiago@gmail.com>
    
    ---------
    
    Co-authored-by: Bruna Santos <brunassdev@gmail.com>
    Co-authored-by: Tiago de Andrade Freire <tiago.freire@cubos.io>
    Co-authored-by: Tiago de Andrade Freire <freire.tiago@gmail.com>
    Co-authored-by: BrunaCubos <104789782+BrunaCubos@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Provides a route for seller get paginated list of quotes at marketplace
+
 ## [2.7.0] - 2025-01-09
 
 ### Added
--- node/resolvers/utils/sellerQuotesController.ts
+++ node/resolvers/utils/sellerQuotesController.ts
@@ -1,4 +1,5 @@
 import { NotFoundError } from '@vtex/api'
+import pLimit from 'p-limit'
 
 import {
   QUOTE_DATA_ENTITY,
@@ -23,21 +24,24 @@ export default class SellerQuotesController {
   private async getSellerQuotes({
     page = 1,
     pageSize = 1,
-    where,
-    sort,
+    where = '',
+    sort = '',
   }: GetQuotesArgs) {
-    return this.ctx.clients.masterdata.searchDocuments<Quote>({
-      dataEntity: QUOTE_DATA_ENTITY,
-      fields: QUOTE_FIELDS,
-      schema: SCHEMA_VERSION,
-      pagination: { page, pageSize },
-      where: `seller=${this.seller} AND (${where})`,
-      sort,
-    })
+    return this.ctx.clients.masterdata.searchDocumentsWithPaginationInfo<Quote>(
+      {
+        dataEntity: QUOTE_DATA_ENTITY,
+        fields: QUOTE_FIELDS,
+        schema: SCHEMA_VERSION,
+        pagination: { page, pageSize },
+        where: `seller=${this.seller} AND (${where})`,
+        sort,
+      }
+    )
   }
 
   private async getSellerQuote(id: string) {
-    const [quote] = await this.getSellerQuotes({ where: `id=${id}` })
+    const { data } = await this.getSellerQuotes({ where: `id=${id}` })
+    const [quote] = data
 
     if (!quote) {
       throw new NotFoundError('seller-quote-not-found')
@@ -62,5 +66,43 @@ export default class SellerQuotesController {
     )
 
     return { ...quote, organizationName, costCenterName }
+  }
+
+  public async getSellerQuotesPaginated({
+    page,
+    pageSize,
+    where,
+    sort = 'creationDate DESC',
+  }: {
+    page: number
+    pageSize: number
+    where?: string
+    sort?: string
+  }) {
+    const { data, pagination } = await this.getSellerQuotes({
+      page,
+      pageSize,
+      where,
+      sort,
+    })
+
+    const limit = pLimit(15)
+    const enrichedQuotes = await Promise.all(
+      data.map((quote) =>
+        limit(async () => {
+          const {
+            organizationName,
+            costCenterName,
+          } = await this.getOrganizationData(quote)
+
+          return { ...quote, organizationName, costCenterName }
+        })
+      )
+    )
+
+    return {
+      data: enrichedQuotes,
+      pagination,
+    }
   }
 }
